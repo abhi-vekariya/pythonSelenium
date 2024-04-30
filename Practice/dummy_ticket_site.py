474d1f57a912e3d92c41b4ca9cd43ae08defd839
abhi.simform <abhi.v@simformsolutions.com>
2024-04-30 23:30:33 +0530

    working on Visa Application site, most of content of page got automated.

--- Practice/dummy_ticket_site.py
+++ Practice/dummy_ticket_site.py
@@ -2,26 +2,104 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 serv_obj = Service("/home/abhivekariya/driver_for_browser/chrome/chromedriver")
 driver = webdriver.Chrome(service=serv_obj)
+driver.implicitly_wait(5)
 
 driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
 driver.maximize_window()
 
-radio_buttons = driver.find_elements(By.XPATH, "//ul[@id='checkout-products']")
+radio_buttons = driver.find_elements(By.XPATH, "//ul[@id='checkout-products']/li")
+total_radio_buttons = len(radio_buttons)
 
-desired_option = "Dummy ticket and hotel"
-for radio in radio_buttons:
-    option = driver.find_element(By.XPATH,"//input[@id='product_3186']").text
-    if option == desired_option:
-        radio.click()
-        break
+desired_option = "Dummy ticket for Visa Application"
+for r in range(1, total_radio_buttons + 1):
+    main_text = driver.find_element(By.XPATH, "//ul[@id='checkout-products']/li[" + str(r) + "]").text
+    real_text = main_text.split("  —  ")[0]
+    if real_text == desired_option:
+        driver.find_element(By.XPATH, "//ul[@id='checkout-products']/li[" + str(r) + "]/label").click()
+
+driver.find_element(By.CSS_SELECTOR, "input[id='travname']").send_keys("Kaushik")
+driver.find_element(By.ID, "travlastname").send_keys("Patel")
+
+# Find using nested element(tag.class tag#ID)
+driver.find_element(By.CSS_SELECTOR, "span.woocommerce-input-wrapper textarea#order_comments").send_keys(
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+
+driver.find_element(By.XPATH, "//input[@id='dob']").click()
+
+dropdown_month = Select(driver.find_element(By.CSS_SELECTOR, "select.ui-datepicker-month"))
+dropdown_month.select_by_visible_text("Dec")
+
+dropdown_year = Select(driver.find_element(By.CSS_SELECTOR, "select.ui-datepicker-year"))
+dropdown_year.select_by_visible_text("2002")
+
+days = driver.find_elements(By.XPATH, "//table[@class='ui-datepicker-calendar']/tbody/tr/td")
+
+for day in days:
+    if day.text == "9":
+        day.click()
+
+Gender = driver.find_elements(By.CSS_SELECTOR, "input[type='radio'][name='sex'] + label")
+
+for g in Gender:
+    if g.text == 'Female':
+        g.click()
+
+driver.find_element(By.XPATH, "//input[@id='addmorepax']").click()
+driver.find_element(By.XPATH, "//input[@id='traveltype_2']").click()
+driver.find_element(By.XPATH, "//input[@id='fromcity']").send_keys("Mumbai")
+driver.find_element(By.XPATH, "//input[@id='tocity']").send_keys("Delhi")
+
+driver.find_element(By.XPATH, "//input[@id='departon']").click()
+
+departure_month = Select(driver.find_element(By.XPATH, "//select[@aria-label='Select month']"))
+departure_month.select_by_visible_text("Aug")
+
+departure_year = Select(driver.find_element(By.XPATH, "//select[@aria-label='Select year']"))
+departure_year.select_by_visible_text("2024")
+
+departure_dates = driver.find_elements(By.XPATH, "//table[@class='ui-datepicker-calendar']/tbody/tr/td")
+
+for d_date in departure_dates:
+    if d_date.text == "22":
+        d_date.click()
+
+driver.find_element(By.XPATH, "//input[@id='returndate']").click()
+
+return_month = Select(driver.find_element(By.XPATH, "//select[@aria-label='Select month']"))
+return_month.select_by_visible_text("Nov")
+
+return_year = Select(driver.find_element(By.XPATH, "//select[@aria-label='Select year']"))
+return_year.select_by_visible_text("2024")
+
+return_dates = driver.find_elements(By.XPATH, "//table[@class='ui-datepicker-calendar']/tbody/tr/td")
+
+for r_date in return_dates:
+    if r_date.text == "13":
+        r_date.click()
+
+driver.find_element(By.XPATH, "//textarea[@name='notes']").send_keys(
+    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and"
+    "1.10.33 of de Finibus Bonorum et Malorum(The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.."
+    ", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.")
+
+driver.find_element(By.CSS_SELECTOR, "#select2-reasondummy-container").click()
+driver.find_element(By.XPATH,"//input[@role='combobox']").send_keys("Car")
+driver.find_element(By.XPATH,"//li[contains(text(), 'Car rental')]").click()
 
 
-
+# desired_option = "WhatsApp"
+# for radio in range(1,total_buttons+1):
+#      option = driver.find_element(By.XPATH,"//p[@id='deliverymethod_field']/span/input["+str(radio)+"]")
+#      print(option.text)
+#      if option.text == desired_option:
+#          option.click()
+#          break
 
 #     # Identify the radio buttons
 #     radio_buttons = driver.find_elements(By.XPATH, "//ul[@id='checkout-products']")
